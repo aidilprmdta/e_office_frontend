a6e18925e81b05d85743cac2710da230c64d8332
aidilprmdta <sekaiiprojectmv@gmail.com>
2026-05-25 21:58:55 +0700

    🔥 feat – import semua isi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export { default as Login } from "./Login";
 export { default as Dashboard } from "./Dashboard";
 export { default as SuratMasuk } from "./SuratMasuk";
-export { default as SuratKeluar } from "./SuratKeluar";
+export { default as SuratKeluar } from "./RiwayatPengajuan";
 export { default as TugasAkhir } from "./TugasAkhir";
 export { default as Persetujuan } from "./Persetujuan";
 export { default as ManajemenUser } from "./ManajemenUser";
